1f15807abd72adc2dd0630a1e93c00840243881a
Francois-Xavier Le Bail <fx.lebail@yahoo.com>
2015-09-14 17:16:26 +0200

    Remove a non-dissection related field from struct netdissect_options
    Remove 'ndo_immediate' from netdissect_options, only used is frontend.
    Use instead 'immediate_mode' variable in frontend.

--- tcpdump.c
+++ tcpdump.c
@@ -741,12 +741,12 @@ main(int argc, char **argv)
 	int pflag=0;			/* don't go promiscuous */
 	netdissect_options Ndo;
 	netdissect_options *ndo = &Ndo;
+	int immediate_mode = 0;
 
 	memset(ndo, 0, sizeof(*ndo));
 	ndo->ndo_dlt=-1;
 	ndo_set_function_pointers(ndo);
 	ndo->ndo_snaplen = DEFAULT_SNAPLEN;
-	ndo->ndo_immediate = 0;
 
 	cnt = -1;
 	device = NULL;
@@ -1161,7 +1161,7 @@ main(int argc, char **argv)
 
 #ifdef HAVE_PCAP_SET_IMMEDIATE_MODE
 		case OPTION_IMMEDIATE_MODE:
-			ndo->ndo_immediate = 1;
+			immediate_mode = 1;
 			break;
 #endif
 
@@ -1208,7 +1208,7 @@ main(int argc, char **argv)
 	 * probably expecting to see packets pop up immediately.
 	 */
 	if (WFileName == NULL && isatty(1))
-		ndo->ndo_immediate = 1;
+		immediate_mode = 1;
 #endif
 
 #ifdef WITH_CHROOT
@@ -1337,7 +1337,7 @@ main(int argc, char **argv)
 #endif
 
 #ifdef HAVE_PCAP_SET_IMMEDIATE_MODE
-		if (ndo->ndo_immediate) {
+		if (immediate_mode) {
 			status = pcap_set_immediate_mode(pd, 1);
 			if (status != 0)
 				error("%s: Can't set immediate mode: %s",

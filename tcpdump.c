d1a54d87d6bbd9ddc09ead4264d9ac03f346dcf7
Francois-Xavier Le Bail <fx.lebail@yahoo.com>
2015-12-15 22:12:43 +0100

    Fix some leaks found by Valgrind/Memcheck

--- tcpdump.c
+++ tcpdump.c
@@ -1485,6 +1485,7 @@ main(int argc, char **argv)
 		bpf_dump(&fcode, dflag);
 		pcap_close(pd);
 		free(cmdbuf);
+		pcap_freecode(&fcode);
 		exit(0);
 	}
 	init_print(ndo, localnet, netmask, timezone_offset);
@@ -1781,6 +1782,7 @@ main(int argc, char **argv)
 	while (ret != NULL);
 
 	free(cmdbuf);
+	pcap_freecode(&fcode);
 	exit(status == -1 ? 1 : 0);
 }
 

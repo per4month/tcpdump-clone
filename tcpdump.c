527a6d77e0d7faf0014ec7c7f19ed509d83fa239
Francois-Xavier Le Bail <fx.lebail@yahoo.com>
2015-12-17 22:04:09 +0100

    There is no need to use strdup() for options
    The other options don't use it.

--- tcpdump.c
+++ tcpdump.c
@@ -1118,15 +1118,11 @@ main(int argc, char **argv)
 			break;
 #endif
 		case 'z':
-			zflag = strdup(optarg);
-			if (zflag == NULL)
-				error("Unable to allocate memory for -z argument");
+			zflag = optarg;
 			break;
 
 		case 'Z':
-			username = strdup(optarg);
-			if (username == NULL)
-				error("Unable to allocate memory for -Z argument");
+			username = optarg;
 			break;
 
 		case '#':

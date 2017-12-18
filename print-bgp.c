6bc504f45a255281ab2525725561da1777ac8e0c
Guy Harris <guy@alum.mit.edu>
2017-12-18 12:29:08 -0800

    Get rid of an unused variable.

--- print-bgp.c
+++ print-bgp.c
@@ -3209,7 +3209,6 @@ static void
 bgp_update_print(netdissect_options *ndo,
                  const u_char *dat, u_int length)
 {
-    const struct bgp *bgp_header;
     const u_char *p;
     u_int withdrawn_routes_len;
     char buf[MAXHOSTNAMELEN + 100];
@@ -3222,7 +3221,6 @@ bgp_update_print(netdissect_options *ndo,
     ND_TCHECK_LEN(dat, BGP_SIZE);
     if (length < BGP_SIZE)
         goto trunc;
-    bgp_header = (const struct bgp *)dat;
     p = dat + BGP_SIZE;
     length -= BGP_SIZE;
 

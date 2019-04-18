086f8843fb99e8272a6c3e6717a8ecb734d62d38
Guy Harris <guy@alum.mit.edu>
2019-04-17 20:21:53 -0700

    Squelch more warnings.

--- print-lldp.c
+++ print-lldp.c
@@ -1162,7 +1162,7 @@ lldp_private_dcbx_print(netdissect_options *ndo,
 {
     int hexdump = FALSE;
     u_int subtype;
-    uint8_t tval;
+    uint16_t tval;
     uint16_t tlv;
     uint32_t i, pgval, uval;
     u_int tlen, tlv_type;

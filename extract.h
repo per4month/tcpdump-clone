194ee07e2e4ff0d3fe5a095e39f80655130f82cc
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2020-09-18 13:17:39 +0200

    Use a switch to manage the setjmp() return values
    Also rename 'ndo_truncated' to 'ndo_early_end'.
    
    The current case (truncated packet) uses ND_TRUNCATED value.
    
    Prepare to add other cases when the current packet cannot be processed
    any more ('Invalid' cases, etc.).

--- extract.h
+++ extract.h
@@ -552,7 +552,7 @@ EXTRACT_IPV4_TO_NETWORK_ORDER(const void *p)
 static inline NORETURN void
 nd_trunc(netdissect_options *ndo)
 {
-	longjmp(ndo->ndo_truncated, 1);
+	longjmp(ndo->ndo_early_end, ND_TRUNCATED);
 }
 
 /* get_u_1 and get_s_1 */

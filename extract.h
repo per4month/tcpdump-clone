9301e3f2541ad5a31a3c73c660b83d4bdd900e23
Denis Ovsienko <denis@ovsienko.info>
2021-08-15 10:54:50 +0100

    Use __builtin_unreachable(). [skip ci]
    Same as in libpcap.
    
    (cherry picked from commit 604cd4aa60557f23ee76a60fd8ea339ca2e54e77)

--- extract.h
+++ extract.h
@@ -37,6 +37,7 @@
  */
 #include "funcattrs.h"
 #include "netdissect.h"
+#include "diag-control.h"
 
 /*
  * If we have versions of GCC or Clang that support an __attribute__
@@ -559,7 +560,7 @@ nd_trunc_longjmp(netdissect_options *ndo)
 	 * XL C that the function is noreturn, but GCC remains unaware of that and
 	 * yields a "'noreturn' function does return" warning.
 	 */
-	while (1);
+	ND_UNREACHABLE
 #endif /* _AIX */
 }
 

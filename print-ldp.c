the-tcpdump-group/tcpdump

Put "}" at beginning of line with "else" to keep a consistent style
[skip ci]

print-ldp.c
@@ -364,8 +364,7 @@ ldp_tlv_print(netdissect_options *ndo,
 		    ND_PRINT(": IPv4 prefix (invalid length)");
 		else
 		    ND_PRINT(": IPv4 prefix %s", buf);
-	    }
-	    else if (af == AFNUM_IP6) {
+	    } else if (af == AFNUM_IP6) {
 		i=decode_prefix6(ndo, tptr, tlv_tlen, buf, sizeof(buf));
 		if (i == -2)
 		    goto trunc;
@@ -375,8 +374,7 @@ ldp_tlv_print(netdissect_options *ndo,
 		    ND_PRINT(": IPv6 prefix (invalid length)");
 		else
 		    ND_PRINT(": IPv6 prefix %s", buf);
-	    }
-	    else
+	    } else
 		ND_PRINT(": Address family %u prefix", af);
 	    break;
 	case LDP_FEC_HOSTADDRESS:

print-pim.c
@@ -599,8 +599,7 @@ pimv2_addr_print(netdissect_options *ndo,
 		if (af == AFNUM_IP) {
 			if (!silent)
 				ND_PRINT("%s", GET_IPADDR_STRING(bp));
-		}
-		else if (af == AFNUM_IP6) {
+		} else if (af == AFNUM_IP6) {
 			if (!silent)
 				ND_PRINT("%s", GET_IP6ADDR_STRING(bp));
 		}
@@ -616,8 +615,7 @@ pimv2_addr_print(netdissect_options *ndo,
 				if (GET_U_1(bp + 1) != 32)
 					ND_PRINT("/%u", GET_U_1(bp + 1));
 			}
-		}
-		else if (af == AFNUM_IP6) {
+		} else if (af == AFNUM_IP6) {
 			if (!silent) {
 				ND_PRINT("%s", GET_IP6ADDR_STRING(bp + 2));
 				if (GET_U_1(bp + 1) != 128)

per4month/tcpdump-clone

(backported from commit 113ed15b7ecae4a3523454344f9b3438ea97fdc7)

print-ldp.c
@@ -364,8 +364,7 @@ ldp_tlv_print(netdissect_options *ndo,
 		    ND_PRINT(": IPv4 prefix (invalid length)");
 		else
 		    ND_PRINT(": IPv4 prefix %s", buf);
-	    }
-	    else if (af == AFNUM_INET6) {
+	    } else if (af == AFNUM_INET6) {
 		i=decode_prefix6(ndo, tptr, tlv_tlen, buf, sizeof(buf));
 		if (i == -2)
 		    goto trunc;
@@ -375,8 +374,7 @@ ldp_tlv_print(netdissect_options *ndo,
 		    ND_PRINT(": IPv6 prefix (invalid length)");
 		else
 		    ND_PRINT(": IPv6 prefix %s", buf);
-	    }
-	    else
+	    } else
 		ND_PRINT(": Address family %u prefix", af);
 	    break;
 	case LDP_FEC_HOSTADDRESS:

print-pim.c
@@ -599,8 +599,7 @@ pimv2_addr_print(netdissect_options *ndo,
 		if (af == AF_INET) {
 			if (!silent)
 				ND_PRINT("%s", GET_IPADDR_STRING(bp));
-		}
-		else if (af == AF_INET6) {
+		} else if (af == AF_INET6) {
 			if (!silent)
 				ND_PRINT("%s", GET_IP6ADDR_STRING(bp));
 		}
@@ -616,8 +615,7 @@ pimv2_addr_print(netdissect_options *ndo,
 				if (GET_U_1(bp + 1) != 32)
 					ND_PRINT("/%u", GET_U_1(bp + 1));
 			}
-		}
-		else if (af == AF_INET6) {
+		} else if (af == AF_INET6) {
 			if (!silent) {
 				ND_PRINT("%s", GET_IP6ADDR_STRING(bp + 2));
 				if (GET_U_1(bp + 1) != 128)